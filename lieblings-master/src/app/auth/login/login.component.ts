62d624d7a2e8891f5e2916a8ee4a6673d2a1387d
thuyle-23 <thuyltt@ctnpsolutions.com>
2022-11-04 23:02:41 +0700

    strength-pass

--- lieblings-master/src/app/auth/login/login.component.ts
+++ lieblings-master/src/app/auth/login/login.component.ts
@@ -9,14 +9,14 @@ export class LoginComponent implements OnInit {
   public account = {
     email: <string>null,
     password: <string>null
-};
-email = '';
-password = '';
-confirmPassword = '';
-valid = {
-  email: true,
-  password: true,
-};
+  };
+  email = '';
+  password = '';
+  confirmPassword = '';
+  valid = {
+    email: true,
+    password: true,
+  };
   public barLabel: string = "Password strength:";
   public myColors = ['#DD2C00', '#FF6D00', '#FFD600', '#AEEA00', '#00C853'];
   public thresholds = [90, 75, 45, 25];
